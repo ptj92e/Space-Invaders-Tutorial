48dc8420dda281b5c47cea2c8590459367268468
Phillip Jones <ptj92e@gmail.com>
2020-06-11 16:34:29 -0500

    Added random spawning enemy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 # Create The Screen
@@ -9,9 +10,18 @@ pygame.display.set_caption("Space Invaders Tutorial")
 playerImg = pygame.image.load("spaceship.png")
 playerX = 370
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+enemyImg = pygame.image.load("enemy.png")
+enemyX = random.randint(0, 736)
+enemyY = random.randint(50, 150)
+enemyX_change = 0
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 # Game Loop
 running = True
@@ -20,9 +30,26 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change -= 3
+            if event.key == pygame.K_RIGHT:
+                playerX_change += 3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
     screen.fill((0, 0, 0))
-
-    player()
     
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
+
     pygame.display.update()
