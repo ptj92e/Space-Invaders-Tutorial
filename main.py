2e5aa6bb7b2ed35186037621229c568702ae3586
Phillip Jones <ptj92e@gmail.com>
2020-06-11 17:05:22 -0500

    Spaceship is shooting bullets

--- main.py
+++ main.py
@@ -24,11 +24,23 @@ enemyX = random.randint(0, 736)
 enemyY = random.randint(50, 150)
 enemyX_change = 3
 
+# Bullet
+bulletImg = pygame.image.load("bullet.png")
+bulletX = 0
+bulletY = 480
+bulletY_change = 10
+bullet_state = "ready"
+
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 def enemy(x, y):
     screen.blit(enemyImg, (x, y))
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
 
 # Game Loop
 running = True
@@ -43,6 +55,8 @@ while running:
                 playerX_change -= 3
             if event.key == pygame.K_RIGHT:
                 playerX_change += 3
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -66,6 +80,11 @@ while running:
         enemyX = 736
         enemyX_change = -3
         enemyY += 40
+
+    # Bullet Movement
+    if bullet_state is "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
